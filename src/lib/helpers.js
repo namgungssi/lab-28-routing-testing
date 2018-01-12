a128a65c537d61c194f8a5d9656c8b16e5aeeed5
dalhun namgung <bnamgung@gmail.com>
2018-01-11 19:40:40 -0800

    working on

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -6,23 +6,17 @@ export const renderIf = (test, component) => {
     return test ? component : undefined
 }
 
-
-
 export const saveNote = (note) => {
     let notes = getAllNotes();
     notes.push(note);
     localStorage.setItem("notes", JSON.stringify(notes));
 }
 
-
-
 export const getNote = (id) => {
   let notes = getAllNotes();
   let note = notes.filter(note => note.id === id);
   return note[0];
 }
-
-
 
 export const removeNote = (id) => {
   let notes = getAllNotes();
@@ -32,16 +26,12 @@ export const removeNote = (id) => {
   localStorage.setItem("notes", JSON.stringify(updatedNotes));
 }
 
-
-
 export const updateNote = (updatedNote) => {
   let current = getAllNotes();
   let updated = current.filter(note => delete note.id === updatedNote.id)
   updated.push(updatedNote);
   localStorage.setItem("notes", JSON.stringify(updated));
 }
-
-
 
 export const getAllNotes = () => {
   return JSON.parse(localStorage.getItem("notes") || "[]");
